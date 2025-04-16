6ba741ba2beb334391fe715ce98f47ec4e1e55db
viktorevgeniev <viktor.evgeniev@gmail.com>
2025-04-16 21:40:19 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def handle_text():
+    text = request.json['text']
+    transform_type = request.json['transform']
+
+    if transform_type == 'reverse':
+        transformed_text = reversed(text)
+    elif transform_type == 'uppercase':
+        transformed_text = text.upper()
+    elif transform_type == 'lowercase':
+        transformed_text = text.lower()
+    elif transform_type == 'wordcount':
+        wordcount = len(text.split())
+        transformed_text = jsonify({'wordcount': wordcount})
+    else:
+        return jsonify({'error': 'Unknown transform type'}), 400
+
+    return jsonify({'transformed_text': transformed_text})
+
+if __name__ == '__main__':
+    app.run(debug=True)
